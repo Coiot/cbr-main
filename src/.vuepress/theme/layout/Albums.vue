bde729cd271c521e643f49ba474ad5a88b5d1191
Isaac Volpe <isaac.volpe@gmail.com>
2019-06-22 00:56:12 -0500

    Metas and Alt Texts

--- src/.vuepress/theme/layout/Albums.vue
+++ src/.vuepress/theme/layout/Albums.vue
@@ -32,7 +32,7 @@
       <p class="abstract" tabindex="0">{{ $page.frontmatter.abstract }}</p>
       <div class="medium" v-for="scene in $page.frontmatter.scenes" :key="$page.frontmatter.scenes">
         <h2 tabindex="0">{{ scene.scene_number }}</h2>
-        <img v-lazy="scene.slide_url" tabindex="0">
+        <img v-lazy="scene.slide_url" tabindex="0" alt="CBR In-Game Screenshot">
         <h3>{{ scene.scene_title }}</h3>
         <div class="narrations" v-html="scene.narration" tabindex="0"></div>
         </li>
