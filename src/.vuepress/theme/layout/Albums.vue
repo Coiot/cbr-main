5ea806ced2ad30a4089862d1932fb00aa392c174
Isaac Volpe <isaac.volpe@gmail.com>
2020-02-13 10:36:53 -0600

    Add

--- src/.vuepress/theme/layout/Albums.vue
+++ src/.vuepress/theme/layout/Albums.vue
@@ -31,8 +31,8 @@
     <h2 class="scenenumber" v-if="$page.frontmatter.description">Abstract</h2>
     <p class="abstract" tabindex="0" v-if="$page.frontmatter.description">{{ $page.frontmatter.description }}</p>
 
-    <section class="scenes" id="lightgallery">
-      <div class="medium" v-for="scene in $page.frontmatter.scenes" :key="$page.frontmatter.scenes">
+    <section class="scenes">
+      <div id="lightgallery" class="medium" v-for="scene in $page.frontmatter.scenes" :key="$page.frontmatter.scenes">
         <h2 class="scenenumber" v-bind:class="{ civdeathBorder: scene.death }">{{ scene.scene_number }}</h2>
         <img v-lazy="scene.slide_url" tabindex="0" alt="CBR In-Game Screenshot" v-bind:class="{ civdeathImage: scene.death }">
         <h3>{{ scene.scene_title }}</h3>
@@ -46,6 +46,9 @@
   </div>
 </transition>
 <script src="js/lightgallery.min.js"></script>
+<script type="text/javascript">
+  lightGallery(document.getElementById('lightgallery'));
+</script>
 </template>
 
 <script>
