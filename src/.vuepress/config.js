98ad12d88203dc21e8d21e9ed16fe7afb902df3c
Isaac Volpe <isaac.volpe@gmail.com>
2020-08-18 20:11:32 -0500

    Config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -69,6 +69,5 @@ module.exports = {
 		['meta', { name: 'msapplication-TileColor', content: '#FFBF46' }],
 		['meta', { name: 'theme-color', content: '#FFBF46' }],
 		['link', { rel: 'canonical', href: 'https://civbattleroyale.tv' }],
-		['meta', { name: 'viewport', content: 'initial-scale=1.0' }],
 	]
 }
