6dda8737ec6a3c34fa0b73f750d2cbbf6922b8d7
Isaac Volpe <isaac.volpe@gmail.com>
2021-06-22 14:26:47 +0000

    Icons

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -55,7 +55,7 @@ module.exports = {
   head: [
     [
       "link",
-      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-icon.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
     ],
     ["link", { rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" }],
     ["link", { rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" }],
