27361c32b099d9db4ea2cc86df19f457a6ac33c7
Isaac Volpe <isaac.volpe@gmail.com>
2024-07-05 01:42:42 +0000

    +

---
+++ src/.vuepress/enhanceApp.js
@@ -0,0 +1,16 @@
+export default ({ router, siteData }) => {
+	// This ensures the script runs only on the client side
+	if (typeof window !== 'undefined') {
+		const script = document.createElement('script');
+		script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+		script.onload = () => {
+			kofiWidgetOverlay.draw('coiot', {
+				'type': 'floating-chat',
+				'floating-chat.donateButton.text': 'Support Us',
+				'floating-chat.donateButton.background-color': '#fcbf47',
+				'floating-chat.donateButton.text-color': '#323842'
+			});
+		};
+		document.body.appendChild(script);
+	}
+};
